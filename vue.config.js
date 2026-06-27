f5d92e7585098b9feb7b0e7b62df4fe66da7607d
a18361272323 <a18361272323@github.com>
2026-06-27 22:41:47 +0800

    Fix: ProvidePlugin for defineComponent, concatModules false

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
 module.exports = defineConfig({
   transpileDependencies: ['@jiaminghi/data-view'],
   lintOnSave: false,
@@ -6,6 +8,15 @@ module.exports = defineConfig({
   parallel: false,
   publicPath: 'https://cdn.jsdelivr.net/gh/a18361272323/dashboard_kpi@688ef81/dist/',
   configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+      new webpack.ProvidePlugin({
+        defineComponent: ['vue', 'defineComponent'],
+      }),
+    ],
     optimization: { splitChunks: false, concatenateModules: false },
     output: { filename: 'js/app.js', chunkFilename: 'js/[name].js' }
   },
