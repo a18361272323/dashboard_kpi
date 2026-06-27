61f3d8eb0ce88da7d83ead86a0cd9ea7684de5e7
a18361272323 <a18361272323@github.com>
2026-06-27 21:53:59 +0800

    Build: single bundle, CDN publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: ['@jiaminghi/data-view'],
   lintOnSave: false,
   runtimeCompiler: true,
-  parallel: false
+  parallel: false,
+  publicPath: 'https://cdn.jsdelivr.net/gh/a18361272323/dashboard_kpi@latest/dist/',
+  configureWebpack: {
+    optimization: { splitChunks: false },
+    output: { filename: 'js/app.js', chunkFilename: 'js/[name].js' }
+  },
+  css: { extract: { filename: 'css/app.css' } }
 })
