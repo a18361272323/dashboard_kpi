43017acfd61df23810ac1171fdf8f88666addf8a
a18361272323 <a18361272323@github.com>
2026-06-27 22:01:07 +0800

    Fix: createMemoryHistory (no History API in srcdoc)

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   publicPath: 'https://cdn.jsdelivr.net/gh/a18361272323/dashboard_kpi@latest/dist/',
   configureWebpack: {
     optimization: { splitChunks: false },
-    output: { filename: 'js/app.js', chunkFilename: 'js/[name].js' }
+    output: { filename: 'js/app.js', chunkFilename: 'js/[name].[contenthash:8].js' }
   },
   css: { extract: { filename: 'css/app.css' } }
 })
